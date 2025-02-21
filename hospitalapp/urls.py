ca5e7f06f59654ecce3e877e137b47417d1534a7
kirumbastacy25l <minniebooh7@gmail.com>
2025-02-21 13:27:26 +0300

    initial commit

---
+++ hospitalapp/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from hospitalapp import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index,name = 'home'),
+    path('service/', views.service,name='services'),
+    path('starter/', views.starter,name = 'starter'),
+]
